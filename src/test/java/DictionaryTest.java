97547660ee3254713f8e59d12d25231b4f68fa5f
selimbr <59741733+selimbr@users.noreply.github.com>
2021-10-28 09:12:58 +0200

    1

--- src/test/java/DictionaryTest.java
+++ src/test/java/DictionaryTest.java
@@ -3,6 +3,13 @@ import org.junit.Test;
 
 
 public class DictionaryTest {
+
+    Dictionary dico;
+
+    @Test public void testDictionaryName() {
+        dic=new Dictionary("Example");
+        assertThat(dict.getName(), equalTo("Example"));
+    }
     @Test public void doNthing(){
         Assert.assertFalse(false);
     }
