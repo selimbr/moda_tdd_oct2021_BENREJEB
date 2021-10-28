d3e16018bdb7deb08d7e8c61f40283cb438d8b0b
selimbr <59741733+selimbr@users.noreply.github.com>
2021-10-28 14:37:52 +0200

    testDic addTranslation v2

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Dictionary {
     private String name;
-    Map<String, String> translations;
+    private Map<String, String> translations;
+    private List<String> traduc;
 
 
     public Dictionary(String name) {
         this.name = name;
         this.translations=new HashMap<>();
+        this.traduc= new ArrayList<String>();
     }
 
     public String getName(){
@@ -19,6 +23,8 @@ public class Dictionary {
     }
     public void addTranslation(String s1,String s2){
 
+        traduc.add(s2);
+        this.translations.put(s1,s2);
 
     }
     public String getTranslation(String s){
